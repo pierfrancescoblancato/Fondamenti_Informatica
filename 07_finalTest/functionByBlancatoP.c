be6bd19d0b809dfd8d9248f8b92d48f826ef1102
pierfrancescoblancato <pierfrancescoblancato436@gmail.com>
2026-04-15 16:10:28 +0200

    add a minor functionByBlancatoP

--- 07_finalTest/functionByBlancatoP.c
+++ 07_finalTest/functionByBlancatoP.c
@@ -1,15 +1,21 @@
+// Library
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
+// MACRO
 #define MAX_PRODUCTS 100
-#define NAME_LENGTH 30
+#define NAME_LENGTH 50
 
-typedef struct{
+#define MIN_MENU 1
+#define MAX_MENU 13
+typedef struct
+{
     int id;
-    char name[50];
-    char brand[50];
-    char category[50];
+    char name[NAME_LENGTH];
+    char brand[NAME_LENGTH];
+    char category[NAME_LENGTH];
     float price;
     int quantity;
     int warranty;
@@ -17,44 +23,65 @@ typedef struct{
 
 } Product;
 
-// Robust function that read a user input
+// By Mirko Di Natale
+void clearBuffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+}
+
+void pause()
+{
+    printf("\nPress Enter to be continue...");
+    getchar();
+    printf("\033[H\033[2J");
+}
+
 // By Pierfrancesco Blancato
-int readInt(const char prompt[]){
+int readInt(const char prompt[])
+{
     int value;
-    while (1){
+    while (1)
+    {
         printf("%s", prompt);
 
-        if (scanf("%d", &value) == 1){
+        if (scanf("%d", &value) == 1)
+        {
+            clearBuffer();
             return value;
         }
 
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF){}
+        clearBuffer();
         printf("Input not valid. Try again.\n");
     }
 }
+
 // By Pierfrancesco Blancato
-float intToFloat(int value){
-    return (float)value/100;
+float intToFloat(int value)
+{
+    return (float)value / 100;
 }
+
 // By Pierfrancesco Blancato
 int readRange(const char prompt[], int min_value, int max_value)
 {
     int value;
-    while (1){
-        printf("%s", prompt);
-        if (scanf("%d", &value) != 1){
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF){}
-            continue;
-        }
+    while (1)
+    {
+        value = readInt(prompt);
 
-        if (value >= min_value && value <= max_value){
+        if (value >= min_value && value <= max_value)
+        {
             return value;
         }
+        printf("Error: value must be between %d and %d.\n", min_value, max_value);
     }
 }
-void printMenu(){
+
+// By Mirko Di Natale
+void printMenu()
+{
     printf("\n--- ELECTRONICS STORE MANAGEMENT ---\n");
     printf("1. Insert new product\n");
     printf("2. Show all products\n");
@@ -71,37 +98,37 @@ void printMenu(){
     printf("13. Exit\n");
 }
 
-// N2 - By Pierfrancesco Blancato
+// N2 - By Pierfrancesco Blancato - Show all products in the warehouse
 void printAllProducts(Product products[], int countProduct){
     if (countProduct == 0){
         printf("No products available.\n");
         return;
-    }
-
+        }
     printf("+-----+----------------------+-----------------+--------------+----------+------------+------------+------------+\n");
     printf("| Cod | Name                 | Brand           | Cat          | Price    | Qty Avail. | Warr. (Mo) | Status     |\n");
     printf("+-----+----------------------+-----------------+--------------+----------+------------+------------+------------+\n");
 
     for (int i = 0; i < countProduct; i++){
         printf("| %-3d | %-20s | %-15s | %-12s | %-8.2f | %-10d | %-10d | %-10s |\n",
-               products[i].id,
-               products[i].name,
-               products[i].brand,
-               products[i].category,
-               products[i].price,
-               products[i].quantity,
-               products[i].warranty,
-               products[i].status ? "Avb" : "Not Avb.");
+        products[i].id,
+        products[i].name,
+        products[i].brand,
+        products[i].category,
+        products[i].price,
+        products[i].quantity,
+        products[i].warranty,
+        products[i].status ? "Avb" : "Not Avb.");
     }
     printf("+-----+----------------------+-----------------+--------------+----------+------------+------------+------------+\n");
 }
 
-// N5 - By Pierfrancesco Blancato
-void addQuantity(Product products[], int countProduct)
+// N5 - By Pierfrancesco Blancato - Update the available quantity of a product in the warehouse
+void addQuantity(Product products[], int countProduct, int searchCode)
 {
-    int index = searchProduct(products, countProduct);
+    int index = searchProduct(products, countProduct, searchCode);
 
-    if (index == -1){
+    if (index == -1)
+    {
         printf("\nProduct not found\n");
         return;
     }
@@ -111,28 +138,38 @@ void addQuantity(Product products[], int countProduct)
     int newQuantity = readInt("Enter a new quantity: ");
     products[index].quantity = newQuantity;
 
+    if (products[index].quantity > 0)
+    {
+        products[index].status = true;
+    }
+    else
+    {
+        products[index].status = false;
+    }
+
     printf("Update Successful -> Code: %d, Name: %s, New Quantity: %d\n\n", products[index].id, products[index].name, products[index].quantity);
 }
 
-// N10 - By Pierfrancesco Blancato
-void countProductForCategory(Product products[], int countProduct){
-    if (countProduct == 0){
+// N10 - By Pierfrancesco Blancato - Count how many products are in a specific category in the warehouse
+void countProductForCategory(Product products[], int countProduct)
+{
+    if (countProduct == 0)
+    {
         printf("No products available.\n");
         return;
     }
+
     char searchCategory[50];
     printf("Enter the category you wish to search: ");
-    scanf("%49s", &searchCategory);
+    scanf("%49s", searchCategory);
+    clearBuffer();
+
     int counter = 0;
 
-    // Iterates through the array, checking each individual product
-    for (int i = 0; i < countProduct; i++){ 
-        
-        // The strcmp function compares the current product's category with the searched one.
-        // If it returns 0, it means the two strings are exactly identical.
-        if (strcmp(products[i].category, searchCategory) == 0){
-            
-            // Match found: increment the product counter by 1
+    for (int i = 0; i < countProduct; i++)
+    {
+        if (strcasecmp(products[i].category, searchCategory) == 0)
+        {
             counter++;
         }
     }
--- 07_finalTest/tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-
-    if (searchProduct(products, position, products[position].id) != -1)
-    {
-        printf("ERROR: ID %d already exists!\n", products[position].id);
-        return position;
-    }
